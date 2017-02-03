f7d3a08f36d0da2b7b6690777b6dfa9957192789
Cris <cristinamotinham@gmail.com>
2017-02-03 11:02:13 +0000

    Adiciona arquivo de conexão de db

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express');
-var mongodb = require('mongodb');
+var db = require('./db');
 var app = express();
-var MongoClient = mongodb.MongoClient;
-
 var url = 'mongodb://localhost:27017/mydb'
-var db;
+
 //TODO
 // [x] pegar link
 // [x] gerar um numero para representar a url
 // [ ] guardar no bd link + novo link
 // [ ] redirect do novo link para o link do bd
 
+app.use('/new/:link', require('./newlink'))
 
-db = MongoClient.connect(url, function (err, database) {
+db.connect(url, function (err, database) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
@@ -22,7 +21,6 @@ db = MongoClient.connect(url, function (err, database) {
         console.log("Listenando da porta 8080");
     });
     
-    database.close()
   }
 });
 
